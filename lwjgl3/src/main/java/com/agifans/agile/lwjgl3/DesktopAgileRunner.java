42778702abd5e886a0324ca5ef65a84ea7b0bc7b
Lance Ewing <lance.ewing@gmail.com>
2023-12-23 22:54:03 +0000

    An initial solution to fix issue #3. It doesn't quite match the original AGI interpreter though, so may need to be changed.

--- lwjgl3/src/main/java/com/agifans/agile/lwjgl3/DesktopAgileRunner.java
+++ lwjgl3/src/main/java/com/agifans/agile/lwjgl3/DesktopAgileRunner.java
@@ -6,57 +6,68 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.TimeUtils;
 
 public class DesktopAgileRunner extends AgileRunner implements Runnable {
-	
-	private Thread interpreterThread;
-	
-	private boolean exit;
-
-	@Override
-	public void start() {
-		interpreterThread = new Thread(this);
-        interpreterThread.start();
-	}
-
-	/**
-	 * Executes the Interpreter instance.
-	 */
-	@Override
-	public void run() {
-	    // Start by loading game. We deliberately do this within the thread and
-	    // not in the main libgdx UI thread.
-	    loadGame();
-	    
-		int nanosPerFrame = (1000000000 / 60);     // 60 times a second.
-		long lastTime = TimeUtils.nanoTime();
-		
-		while (true) {
-			if (exit) {
-			    Gdx.app.exit();
-			    return;
-			}
-			
-			try {
-    			// Perform one tick of the interpreter.
-    			interpreter.tick();
     
-    			// Throttle at expected FPS.
-    			while (TimeUtils.nanoTime() - lastTime <= 0L) {
-    				Thread.yield();
-    			}
-    			
-    			lastTime += nanosPerFrame;
-			}
-			catch (QuitAction qa) {
-			    // QuitAction is thrown when the AGI quit() command is executed.
-			    exit = true;
-			}
-		}
-	}
-	
-	@Override
-	public void stop() {
-		exit = true;
-	}
+    private Thread interpreterThread;
+    
+    private boolean exit;
+
+    @Override
+    public void start() {
+        interpreterThread = new Thread(this);
+        interpreterThread.start();
+    }
+
+    /**
+     * Executes the Interpreter instance.
+     */
+    @Override
+    public void run() {
+        // Start by loading game. We deliberately do this within the thread and
+        // not in the main libgdx UI thread.
+        loadGame();
+        
+        int nanosPerFrame = (1000000000 / 60);     // 60 times a second.
+        long lastTime = TimeUtils.nanoTime();
+        
+        while (true) {
+            if (exit) {
+                Gdx.app.exit();
+                return;
+            }
+            
+            try {
+                // Perform one tick of the interpreter.
+                long beforeTick = TimeUtils.nanoTime();
+                interpreter.tick();
+                long duration = (TimeUtils.nanoTime() - beforeTick);
+                
+                if (duration > nanosPerFrame) {
+                    // If the tick took longer than the time per frame, then most likely
+                    // it was due to the menu, or a text window, or something similar that
+                    // keeps the thread busy. In this scenario, we simply increment lastTime
+                    // by the whole duration. We do not play catch up.
+                    lastTime += duration;
+                }
+                else {
+                    // Throttle at expected FPS.
+                    while (TimeUtils.nanoTime() - lastTime <= 0L) {
+                        Thread.yield();
+                    }
+                    
+                    lastTime += nanosPerFrame;
+                }
+            }
+            catch (QuitAction qa) {
+                // QuitAction is thrown when the AGI quit() command is executed.
+                exit = true;
+            }
+        }
+    }
+    
+    @Override
+    public void stop() {
+        exit = true;
+    }
 
     @Override
     public boolean isRunning() {
