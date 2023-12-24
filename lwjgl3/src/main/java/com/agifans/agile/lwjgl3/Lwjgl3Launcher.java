48c7354b39e325a6b5e257e72f8739a85b571592
Lance Ewing <lance.ewing@gmail.com>
2023-12-24 15:42:00 +0000

    Changed desktop icon to get the AGILE icon.

--- lwjgl3/src/main/java/com/agifans/agile/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/agifans/agile/lwjgl3/Lwjgl3Launcher.java
@@ -28,7 +28,7 @@ public class Lwjgl3Launcher {
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
         configuration.setWindowedMode(960, 600);
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        configuration.setWindowIcon("agile-128x128.png", "agile-64x64.png", "agile-32x32.png", "agile-16x16.png");
         return configuration;
     }
 }
