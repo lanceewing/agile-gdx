e767d073610264de255cb9f037568732b708ff31
Lance Ewing <lance.ewing@gmail.com>
2024-02-15 23:57:02 +0000

    Removed unused import.

--- lwjgl3/src/main/java/com/agifans/agile/lwjgl3/DesktopDialogHandler.java
+++ lwjgl3/src/main/java/com/agifans/agile/lwjgl3/DesktopDialogHandler.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
-import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.filechooser.FileSystemView;
 
