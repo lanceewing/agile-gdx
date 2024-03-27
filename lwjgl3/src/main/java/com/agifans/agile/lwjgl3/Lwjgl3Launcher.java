5f2e3b8cf3332284e3b1f74373a424f2917ae32f
Lance Ewing <lance.ewing@gmail.com>
2024-03-27 22:38:26 +0000

    Desktop version of passing in options to start a game automatically rather than showing the home screen.

--- lwjgl3/src/main/java/com/agifans/agile/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/agifans/agile/lwjgl3/Lwjgl3Launcher.java
@@ -2,23 +2,42 @@ package com.agifans.agile.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import com.agifans.agile.Agile;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
         if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
-        createApplication();
+        createApplication(convertArgsToMap(args));
     }
 
-    private static Lwjgl3Application createApplication() {
+    private static Map<String, String> convertArgsToMap(String[] args) {
+        Map<String, String> argsMap = new HashMap<>();
+        if ((args != null) && (args.length > 0)) {
+            for (String arg : args) {
+                int equalsIndex = arg.indexOf('=');
+                if (equalsIndex != -1) {
+                    String name = arg.substring(0, equalsIndex);
+                    String value = arg.endsWith("=")? "" : arg.substring(equalsIndex + 1);
+                    argsMap.put(name, value);
+                }
+            }
+        }
+        return argsMap;
+    }
+    
+    private static Lwjgl3Application createApplication(Map<String, String> argsMap) {
         DesktopDialogHandler desktopDialogHandler = new DesktopDialogHandler();
     	DesktopAgileRunner desktopAgileRunner = new DesktopAgileRunner(
     	        new DesktopUserInput(), new DesktopWavePlayer(), 
     	        new DesktopSavedGameStore(), new DesktopPixelData(), 
     	        new DesktopVariableData());
         return new Lwjgl3Application(
-                new Agile(desktopAgileRunner, desktopDialogHandler), 
+                new Agile(desktopAgileRunner, desktopDialogHandler, argsMap), 
         		getDefaultConfiguration());
     }
 
@@ -31,7 +50,7 @@ public class Lwjgl3Launcher {
         //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-        configuration.setWindowedMode(960, 600);
+        configuration.setWindowedMode(1600, 1000);
         configuration.setWindowIcon("agile-128x128.png", "agile-64x64.png", "agile-32x32.png", "agile-16x16.png");
         return configuration;
     }
